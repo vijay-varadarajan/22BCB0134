312e2a2b621b11948050c3a21ccd2c6e6d7df1fe
vijay-varadarajan <vijayvaradarajan84@gmail.com>
2022-12-05 12:56:21 +0530

    latest

--- C/project_XO/xo1.c
+++ C/project_XO/xo1.c
@@ -5,45 +5,50 @@
 
 int main()
 {
-    char game[3][3];
-    int win;
+        char game[3][3];
+        int win;
 
-    creategame(game);
-
-    dispgame(game);
-
-    storeX(game);
-
-    dispgame(game);
-
-    for (int n=0; n<4; n++)
-    {
-        // Get input coords and check for invalid inputs
-        storeO(game);
+        creategame(game);
 
         dispgame(game);
-
-        // Evaluate the matrix each time to check if pattern matches
-        win = checker(game);
-        if (win == 1){
-            break;
-        }
 
         storeX(game);
 
         dispgame(game);
 
-        // Evaluate the matrix each time to check if pattern matches
-        win = checker(game);
-        if (win == 1){
-            break;
+        for (int n=0; n<4; n++)
+        {
+            // Get input coords and check for invalid inputs
+            storeO(game);
+
+            dispgame(game);
+
+            // Evaluate the matrix each time to check if pattern matches
+            win = checker(game);
+            if (win == 1){
+                break;
+            }
+
+            storeX(game);
+
+            dispgame(game);
+
+            // Evaluate the matrix each time to check if pattern matches
+            win = checker(game);
+            if (win == 1){
+                break;
+            }
         }
-    }
-    if (win == 0){
-        printf("Game Drawn\n");
-    }
-    printf("-----------------------------------------------------------------------------------------------------------");
-    dispgame(game);
-    printf("-----------------------------------------------------------------------------------------------------------");
+        if (win == 0){
+            printf("Game Drawn\n");
+        }
+        printf("-----------------------------------------------------------------------------------------------------------");
+        dispgame(game);
+        printf("-----------------------------------------------------------------------------------------------------------");
+        if (win == 1 || win == 0){
+            printf("\nEnter 1 to exit: ");
+            scanf("%d", &win);
+            printf("%d", &win);
+        }
 }
 
