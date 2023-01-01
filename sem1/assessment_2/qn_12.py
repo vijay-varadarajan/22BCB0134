41f0f01c9c3e454cfb7c042c38bfe6dd406ae07e
vijay-varadarajan <vijayvaradarajan84@gmail.com>
2023-01-01 21:07:17 +0530

    latest

--- sem1/assessment_2/qn_12.py
+++ sem1/assessment_2/qn_12.py
@@ -1,15 +1,47 @@
+from tabulate import tabulate
+
 key = {1: 'A', 2: 'B', 3: 'B', 4: 'A', 5: 'C'}
-cands = int(input("Number of candidates: "))
-opts = []
+cands = int(input())
+opts = {}
 for i in range(cands):
     choices = input().split()
-    opts.append(tuple(choices))
-print(opts)
+    opts[i] = choices
 
-marks = {}
-for i in range(cands):
-    for j in range(len(opts[i])):
-        if opts[i][j] == 'X':
-            continue
-        elif opts[i][j] == key[j+1]:
-            ...
+scores = {}
+
+for i in range(len(opts)):
+    anss = opts[i]
+    score = 0.00
+    for ans in range(len(anss)):
+        if anss[ans] == key[ans+1]:
+            score += 2
+        elif anss[ans] == 'X':
+            pass
+        else:
+            score -= 0.25
+    scores[i] = score
+
+scores = dict(sorted(scores.items(), key=lambda item: item[1], reverse=True))
+
+output = []
+printed_cands = []
+candidate_num = list(scores.keys())
+for i in range(len(scores)):
+    candidate_appended = False
+    candidates = f'C{candidate_num[i]+1}'
+    
+    for cand in printed_cands:
+        if candidates in cand:
+            candidate_appended = True
+    if candidate_appended:
+        continue
+    for j in range(i+1, len(scores)):
+        if scores[i] == scores[j]:
+            candidates += f',C{candidate_num[j]+1}'
+    printed_cands.append(candidates)
+    tot = '{total_score:.2f}'.format(total_score=scores[i])
+    line = {'rank':i+1, 'Candidates':candidates, 'Total':str(tot)}
+
+    output.append(line)
+
+print(tabulate(output, headers='keys', tablefmt="plain", disable_numparse=True))
