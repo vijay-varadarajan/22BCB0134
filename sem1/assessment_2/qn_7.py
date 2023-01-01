8d72fba58422f838be94b887795beea100d63ed4
vijay-varadarajan <vijayvaradarajan84@gmail.com>
2023-01-01 19:23:23 +0530

    latest

---
+++ sem1/assessment_2/qn_7.py
@@ -0,0 +1,28 @@
+import itertools as it
+
+string = input()
+lucky_num = int(input())
+str_len = int(input())
+
+value = {}
+count = 1
+for let in string:
+    value[let] = count
+    count += 1
+print(value)
+
+combs = list(it.combinations(string, str_len))
+print(combs)
+
+lucky_combs = []
+
+for comb in combs:
+    summ = 0
+    for letter in comb:
+        summ += value[letter]
+    print(summ)
+    if summ == lucky_num:
+        lucky_combs.append(comb)
+
+for comb in lucky_combs:
+    print(*comb)
