7268d61025ba41b9791451bd76c633ae6ca2374f
Vijay Varadarajan <vijayvaradarajan84@gmail.com>
2024-08-07 16:51:51 +0530

    os lab da1

--- test.c
+++ test.c
@@ -1,67 +1,39 @@
 #include <stdio.h>
-#include <string.h>
 
-void clear_input_buffer() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF) { }
-}
+int sol (char *s, char* t) {
+    if (s == NULL || t == NULL) {
+        return -1;
+    }
 
-int max(int a, int b) { return a > b ? a : b; }
+    int count = 0;
+    int total = 0;
 
-void LCS(char* a, char* b){
-    int i, j;
+    for (int i = 0; s[i] != '\0'; i++) {
+        total++;
+    }
 
-    int m = 8, n = 8;
+    for (int i = 1; s[i] != '\0'; i++) {
+        if (s[i] > t[i]) {
+            char temp = s[i];
+            s[i] = t[i];
+            t[i] = temp;
 
-    int C[m+1][n+1];
-
-    for (int i = 0; i < m+1; i++){
-        for(int j = 0; j < n+1; j++){
-            if (i == 0 || j == 0){
-                C[i][j] = 0;
-            }
-            else if (a[i-1] == b[j-1]){
-                C[i][j] = C[i-1][j-1] + 1;
-            }
-            else {
-                C[i][j] = max(C[i-1][j], C[i][j-1]);
-            }
+            count++;
         }
     }
-
-    for (int i = 0; i < m+1; i++){
-        for(int j = 0; j < n+1; j++){
-            printf("%d ", C[i][j]);
-        } printf("\n");
+    
+    if (count > total / 2){
+        count = total - count;
     }
 
-    i = m, j = n;
-    int k = 0;
-    int ipos[n];
-
-    while(i > 0 || j > 0){
-        if (a[i-1] == b[j-1]){
-            ipos[k] = i-1;
-            i--;
-            j--;
-            k++;
-        } else if (C[i][j-1] > C[i-1][j]) {
-            j--;
-        } else {
-            i--;
-        }
-    }
-
-    for(int i = k-1; i >= 0; i--){
-        printf("%c ", a[ipos[i]]);
-    }
+    return count;
 }
 
-int main(void){
-    char a[8] = "ABCDGHKL";
-    char b[8] = "AEDFHRKL";
-
-    LCS(a, b);
-
+int main() {
+    char s[] = "91919";
+    char t[] = "19191";
+    int count = sol(s, t);
+    printf("%d\n", count);
     return 0;
 }
+
